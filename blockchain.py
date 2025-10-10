85ff8d23e2031d979fb34abe21206e35b2c5c17b
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-10-09 23:58:36 -0700

    finished blockchain apis

--- blockchain.py
+++ blockchain.py
@@ -61,7 +61,26 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return ""
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    block = blockchain.new_block(proof)
+
+    response = { 
+        'message': "new block made",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
